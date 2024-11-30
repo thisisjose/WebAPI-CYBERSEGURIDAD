f14117ca936d8624d19b3630a6c4755cfcb3b987
thisisjose <josecinuwu@gmail.com>
2024-11-30 16:07:28 -0600

    Agregar archivos de proyecto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Correo { get; set; }
+
+        public string? Clave { get; set; }
+    }
+}
